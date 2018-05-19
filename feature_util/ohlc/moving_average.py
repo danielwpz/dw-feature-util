ac233374bf09688785fbe0cd0a1924463df409d8
Daniel Wang <danielwpz@gmail.com>
2018-05-19 12:56:20 -0700

    reformat

--- feature_util/ohlc/moving_average.py
+++ feature_util/ohlc/moving_average.py
@@ -8,6 +8,7 @@ class SimpleMovingAverageFeatureProducer(FeatureProducerOHLC):
     e.g.
     For series [1, 2, 3, 4, 5, 6, 7], a window 2 SMA produces [NaN, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5]
     """
+
     def __init__(self, period=5, feature='close', min_period=None, feature_label='sma'):
         FeatureProducerOHLC.__init__(self, feature_label)
         self.feature = feature
@@ -17,7 +18,8 @@ class SimpleMovingAverageFeatureProducer(FeatureProducerOHLC):
     def produce(self, df):
         result = df.copy()
         result['%s_%s_%d' % (self.feature, self.feature_label, self.period)] = \
-            result[self.feature].rolling(window=self.period, min_periods=self.min_period).mean()
+            result[self.feature].rolling(
+                window=self.period, min_periods=self.min_period).mean()
         return result
 
 
@@ -25,6 +27,7 @@ class ExponentialMovingAverageFeatureProducer(FeatureProducerOHLC):
     """
     Produce exponential moving average (EMA) feature.
     """
+
     def __init__(self, period=5, feature='close', min_period=0, adjust=True, feature_label='ema'):
         FeatureProducerOHLC.__init__(self, feature_label)
         self.feature = feature
@@ -35,5 +38,6 @@ class ExponentialMovingAverageFeatureProducer(FeatureProducerOHLC):
     def produce(self, df):
         result = df.copy()
         result['%s_%s_%d' % (self.feature, self.feature_label, self.period)] = \
-            result[self.feature].ewm(span=self.period, min_periods=self.min_period, adjust=self.adjust).mean()
+            result[self.feature].ewm(
+                span=self.period, min_periods=self.min_period, adjust=self.adjust).mean()
         return result
